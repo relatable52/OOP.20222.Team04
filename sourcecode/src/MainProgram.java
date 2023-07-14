8afb87be08aae1b75ab4e171e9a267d9fef037bc
Đoàn Ngọc Cường <cuongmkmtpgoldfinch@gmail.com>
2023-07-15 01:42:58 +0700

    DNCuong_up_MainProgram.java
    Đoàn Ngọc Cường upload MainProgram.java

---
+++ sourcecode/src/MainProgram.java
@@ -0,0 +1,18 @@
+
+import controls.Game;
+import gui.MainWindow;
+
+public class MainProgram {
+	public static void main(String[] args) {
+		Game myGame = new Game();
+		MainWindow myWindow = new MainWindow(myGame);
+		
+		while(true) {
+			while(myWindow.getMode() == 2) {
+				myWindow.redraw();
+				myWindow.playGame();
+			}
+			for(int i=0; i<5000; i++) {}
+		}
+	}
+}
